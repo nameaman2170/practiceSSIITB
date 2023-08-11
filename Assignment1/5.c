71f2ed62aa13f44cec3809ca0695aa8b2f233c9a
Aman Pandey <Aman Pandey>
2023-08-11 13:13:29 +0530

    Added 3 and 4 solutions

---
+++ Assignment1/5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+int main(){
+	while(1){
+		printf("1st line ");
+		printf("2st line ");
+		printf("3st line ");
+		printf("4st line ");
+		printf("5st line ");
+	}
+}
