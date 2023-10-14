41f4b9e3a5b5ed2b1f030b7f583528318b915fac
Aman Pandey <Aman Pandey>
2023-10-14 09:24:23 +0530

    clear

--- mini_project/handles/faculty.h
+++
@@ -1,9 +0,0 @@
-#ifndef FACULTY_H
-#define FACULTY_H
-
-int display_faculty_menu();
-void add_new_course();
-void remove_offered_course();
-void view_enrollments_in_courses();
-void faculty_password_change();
-#endif
